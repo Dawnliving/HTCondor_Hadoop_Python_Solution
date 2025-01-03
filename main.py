0d6d9c5caeab18cd69568a7fd2c076f7f0811551
Dawn Zhong <dawnzhong16@gmail.com>
2025-01-03 15:30:11 +0800

    add first two part of EDA

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json_parse as jsc
+import pandas as pd
+
 
 def import_dataset(file, base):
-    import json_parse as jsc
     project_config = jsc.create_nested_object_from_json('./config.json')
-    import pandas as pd
+
     df = pd.DataFrame()
 
     if file == "main":
@@ -55,48 +57,43 @@ def import_dataset(file, base):
     return df
 
 
-def data_pre_processing(df):
-    import pandas as pd
-
-    country_code = import_dataset("country", "remote")
+def data_pre_processing(country_code, dataset, covid19):
     country_code = country_code[['id', 'alpha2', 'alpha3', 'name']]
     country_code['alpha3'] = country_code['alpha3'].str.upper()
-    df['name'] = 'NULL'
-    for i in range(len(df)):
-        if df.iloc[i]['country'] == 'ALL':
+    dataset['name'] = 'NULL'
+    for i in range(len(dataset)):
+        if dataset.iloc[i]['country'] == 'ALL':
             continue
         else:
             if country_code.loc[
                 country_code['alpha3'] ==
-                    df.iloc[i, df.columns.get_loc('country')], 'name'].empty:
+                dataset.iloc[i, dataset.columns.get_loc('country')], 'name'].empty:
                 name = 'NULL'
             else:
                 name = country_code.loc[
-                    country_code['alpha3'] == df.iloc[i, df.columns.get_loc('country')], 'name'].iloc[0]
+                    country_code['alpha3'] == dataset.iloc[i, dataset.columns.get_loc('country')], 'name'].iloc[0]
 
-            df.iloc[i, df.columns.get_loc('name')] = name
+            dataset.iloc[i, dataset.columns.get_loc('name')] = name
 
     # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#Transitional_reservations
-    for i in df[(df['name'] == 'NULL') & (df['country'] == 'ANT')].index:
-        df.iloc[i, df.columns.get_loc('name')] = "Netherlands Antilles"
+    for i in dataset[(dataset['name'] == 'NULL') & (dataset['country'] == 'ANT')].index:
+        dataset.iloc[i, dataset.columns.get_loc('name')] = "Netherlands Antilles"
 
     # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#Examples
-    for i in df[(df['name'] == 'NULL') & (df['country'] == 'XXX')].index:
-        df.iloc[i, df.columns.get_loc('name')] = "Unspecified Nationality"
+    for i in dataset[(dataset['name'] == 'NULL') & (dataset['country'] == 'XXX')].index:
+        dataset.iloc[i, dataset.columns.get_loc('name')] = "Unspecified Nationality"
 
     # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#Transitional_reservations
-    for i in df[(df['name'] == 'NULL') & (df['country'] == 'ZAR')].index:
-        df.iloc[i, df.columns.get_loc('name')] = "Zaire"
+    for i in dataset[(dataset['name'] == 'NULL') & (dataset['country'] == 'ZAR')].index:
+        dataset.iloc[i, dataset.columns.get_loc('name')] = "Zaire"
 
-    df['date'] = pd.to_datetime(df['date'])
+    dataset['date'] = pd.to_datetime(dataset['date'])
 
-    df['year'] = df['date'].dt.year
-    df['month'] = df['date'].dt.month
+    dataset['year'] = dataset['date'].dt.year
+    dataset['month'] = dataset['date'].dt.month
 
-    df.insert(1, 'year', df.pop('year'))
-    df.insert(2, 'month', df.pop('month'))
-
-    covid19 = import_dataset("covid19", "remote")
+    dataset.insert(1, 'year', dataset.pop('year'))
+    dataset.insert(2, 'month', dataset.pop('month'))
 
     covid19['date'] = pd.to_datetime(covid19['date'])
 
@@ -115,7 +112,7 @@ def data_pre_processing(df):
 
     covid19_selected = covid[['year', 'month', 'covid']]
 
-    df_merged = pd.merge(df, covid19_selected, on=['year', 'month'], how='left')
+    df_merged = pd.merge(dataset, covid19_selected, on=['year', 'month'], how='left')
 
     df_merged['covid'] = df_merged['covid'].fillna(0).astype(int)
 
@@ -129,10 +126,101 @@ def data_pre_processing(df):
     return dfc
 
 
+def top_10_arrivals(top_ten_arrivals):
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import LinearSegmentedColormap
+    colors = ['#32CD32', '#90EE90', '#FFFF00', '#FFD700', '#FFA500', '#FF4500', '#FF0000']
+    custom_cmap = LinearSegmentedColormap.from_list('custom_gradient', colors)
+
+    values = top_ten_arrivals['arrivals_millions']
+    norm = plt.Normalize(values.min(), values.max())
+    colors = custom_cmap(norm(values))
+
+    # Create vertical bar plot
+    plt.figure(figsize=(12, 6))
+    bars = plt.bar(top_ten_arrivals['name'], top_ten_arrivals['arrivals_millions'], color=colors)
+
+    # Customize the plot
+    plt.title('Top 10 arrivals in Malaysia')
+    plt.xlabel('Country')
+    plt.ylabel('Arrivals (Millions)')
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
+
+    # Add value labels on top of the bars
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height,
+                 f'{height:.6f}M',  # Display 6 decimal places
+                 ha='center', va='bottom', fontsize=10)
+
+    # Adjust layout to prevent label cutoff
+    plt.tight_layout()
+
+    # Show the plot
+    # plt.show()
+    plt.savefig("./output/top_10_arrivals.jpg")
+
+
+def top_10_male_arrivals(top_ten_male_arrivals):
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import LinearSegmentedColormap
+
+    colors = ['#ADD8E6', '#87CEEB', '#4169E1', '#1E90FF', '#0000FF', '#00008B']
+    custom_cmap = LinearSegmentedColormap.from_list('custom_red', colors)
+
+    values = top_ten_male_arrivals['arrivals_male_millions']
+    norm = plt.Normalize(values.min(), values.max())
+    colors = custom_cmap(norm(values))
+
+    # Create vertical bar plot
+    plt.figure(figsize=(12, 6))
+    bars = plt.bar(top_ten_male_arrivals['name'], top_ten_male_arrivals['arrivals_male_millions'], color=colors)
+
+    # Customize the plot
+    plt.title('Top 10 male arrivals in Malaysia')
+    plt.xlabel('Country')
+    plt.ylabel('Male Arrivals (Millions)')
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
+
+    # Add value labels on top of the bars
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height,
+                 f'{height:.6f}M',  # Display 6 decimal places
+                 ha='center', va='bottom', fontsize=10)
+
+    # Adjust layout to prevent label cutoff
+    plt.tight_layout()
+
+    # Show the plot
+    plt.savefig("./output/top_10_male_arrivals.jpg")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    dataset = import_dataset("main", "remote")
-    #print(dataset)
-    print(data_pre_processing(dataset))
+    dataset = import_dataset("main", "local")
+    country_code = import_dataset("country", "local")
+    covid19 = import_dataset("covid19", "local")
+
+    # dataset after data preprocessing
+    dfc = data_pre_processing(country_code, dataset, covid19)
+
+    # EDA
+    grouped_arrivals = dfc[dfc['country'] != "ALL"]
+
+    # top_10_arrivals
+    top_ten_arrivals = pd.DataFrame(grouped_arrivals.groupby('name')['arrivals'].sum().nlargest(10)).reset_index()
+    top_ten_arrivals['arrivals_millions'] = top_ten_arrivals['arrivals'] / 1000000
+    top_10_arrivals(top_ten_arrivals)
+
+    # top_10_male_arrivals
+    top_ten_male_arrivals = pd.DataFrame(
+        grouped_arrivals.groupby('name')['arrivals_male'].sum().nlargest(10)).reset_index()
+    top_ten_male_arrivals['arrivals_male_millions'] = top_ten_male_arrivals['arrivals_male'] / 1000000
+    top_10_male_arrivals(top_ten_male_arrivals)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
